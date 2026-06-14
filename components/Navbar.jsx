ca1d37b57a416ae802b67f57c59c72f1df086dad
gokulmm2569-gif <gokulmm2569@gmail.com>
2026-06-14 21:07:48 +0530

    first commit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -42,7 +42,7 @@ export default function Navbar() {
             href="/register"
             className="rounded-full bg-yellow-400 px-4 py-2 font-medium text-black transition hover:bg-yellow-300"
           >
-            Get Started
+            Get to Started
           </Link>
 
 
